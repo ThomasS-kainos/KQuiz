24a516b15e61dde775b909d5052b1d062d9e652e
Thomas Sargent <thomas.sargent@kainos.com>
2026-08-21 10:32:35 +0100

    Added Question Types to the API

--- server/src/SingletonStore/quiz.ts
+++ server/src/SingletonStore/quiz.ts
@@ -3,25 +3,18 @@ import type { Question } from "../types/questions.ts";
 export class Quiz {
     public quizName: string = "Unnamed Quiz";
 
-    public currentQuestion: Question = { question: "", answer: "" };
+    public currentQuestion: Question | null = null;
     public Questions: Array<Question> = [];
 
     constructor(quizName: string) {
         this.quizName = quizName;
-
-        this.Questions = [
-            { question: "What is the capital of France?", answer: "Paris" },
-            { question: "What is 2 + 2?", answer: "4" },
-            { question: "What is the largest ocean on Earth?", answer: "Pacific Ocean" },
-        ];
     }
 
     public NextQuestion() {
         const nextQuestion = this.Questions.shift();
+
         if (nextQuestion) {
             this.currentQuestion = nextQuestion;
-        } else {
-            this.currentQuestion = { question: "No more questions available.", answer: "" };
         }
     }
 }
